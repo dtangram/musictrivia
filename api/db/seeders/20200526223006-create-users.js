b6d6a8890e1428f153fd80c16aa3353aaf647f87
Douglas T. Angram <douglast.angram@douglass-mbp.attlocal.net>
2020-05-26 17:34:12 -0500

    Heroku Upload

---
+++ api/db/seeders/20200526223006-create-users.js
@@ -0,0 +1,20 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.bulkInsert(
+    'Users',
+    [
+      {
+        id: '712acaa6-7f3e-4dd3-96c9-ce74650133c9',
+        username: 'dtangram',
+        email: 'dtangram@gmail.com',
+        password: 'password1234',
+        accesstoken: 'abcd1234',
+        type: 'regular',
+        createdAt: Sequelize.literal('NOW()'),
+        updatedAt: Sequelize.literal('NOW()'),
+      },
+    ],
+    {},
+  ),
+
+  down: queryInterface => queryInterface.bulkDelete('Users', null, {}),
+};
