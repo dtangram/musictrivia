15325c67bc2e3721dfcfd9cd1fecb750fe7c7e84
Douglas T. Angram <douglast.angram@douglass-mbp.attlocal.net>
2020-06-10 16:58:15 -0500

    New MusicTrivia Commit

--- reactjs/src/quizzes/landing/index.js
+++ reactjs/src/quizzes/landing/index.js
@@ -37,7 +37,7 @@ class Landing extends React.Component {
 
           <div className="container">
             <div className="row">
-              {publicQuizzes.map(quiz => (
+              {!userId ? (<h3>No Quizzes</h3>) : publicQuizzes.map(quiz => (
                 <article className="card col-sm-12 col-md-12 col-lg-4" key={quiz.id}>
                   <div className="card-body">
                     <form>
