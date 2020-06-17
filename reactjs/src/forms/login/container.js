cc249f5cb57cfcca02dd0c9e95876b3660565d11
Douglas T. Angram <douglast.angram@douglass-mbp.attlocal.net>
2020-06-17 12:21:40 -0500

    Styles

--- reactjs/src/forms/login/container.js
+++ reactjs/src/forms/login/container.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 // pull actions out that we want to use in this component
-import { redirectToGithub, verifyGitHubCode, loginUser } from '../../store/auth/actions';
+import { verifyGitHubCode, loginUser } from '../../store/auth/actions';
 
 function mapStateToProps(state) {
   // get any information out of the state that we need for this component.
@@ -11,6 +11,6 @@ function mapStateToProps(state) {
 }
 
 // map action functions to props
-const mapDispatchToProps = { redirectToGithub, verifyGitHubCode, loginUser };
+const mapDispatchToProps = { verifyGitHubCode, loginUser };
 // export the redux connected version of the container.
 export default connect(mapStateToProps, mapDispatchToProps);
