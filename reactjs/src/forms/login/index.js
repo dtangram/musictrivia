cab802ea45f080d627dcf8f48c24e5a56c9a8898
Douglas T. Angram <douglast.angram@douglass-mbp.attlocal.net>
2020-06-17 18:11:25 -0500

    Styles

--- reactjs/src/forms/login/index.js
+++ reactjs/src/forms/login/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import RRPropTypes from 'react-router-prop-types';
-// import { Redirect } from 'react-router-dom';
-import { Link as RRLink, Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+// import { Link as RRLink, Redirect } from 'react-router-dom';
 import Link from '../../link';
 import '../../css/main.css';
 import LoginContainer from './container';
@@ -168,7 +168,7 @@ class Login extends React.Component {
 
             <div>
               <p>
-                <RRLink url="" onClick={() => this.redirectToGithub()}>Login with Github</RRLink>
+                <button type="button" onClick={() => this.redirectToGithub()}>Login with Github</button>
                 <br />
                 <Link url="/signup" title="Need an Account?" />
               </p>
